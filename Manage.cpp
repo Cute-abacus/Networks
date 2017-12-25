f8217f7a01b50ada98fcab53b45b07768e1313b8
Cute-abacus <34331076+Cute-abacus@users.noreply.github.com>
2017-12-25 16:45:45 +0800

    Update Manage.cpp

--- Manage.cpp
+++ Manage.cpp
@@ -160,3 +160,43 @@ bool Manage::shortestpath()
 		ShortestPath(Route, ans,path, dist);
 	return true;
 }
+bool Manage::removeEdge()
+{
+	cout << "请输入要删除的边的第一个结点编号\n";
+	int a;
+	cin >> a;
+	cout << "请输入要删除的边的第二个结点编号\n";
+	int b;
+	cin >> b;
+	if (!check(a) || !check(b))
+	{
+		return false;
+	}
+	if (!Route.removeEdge(a, b))
+	{
+		cout << "此边不存在\n";
+		return false;
+	}
+	cout << "删除成功！\n";
+	write2();
+}
+bool Manage::removeVertex()
+{
+	cout << "请输入删除结点的编号\n";
+	int a;
+	cin >> a;
+	if (!check(a))
+	{
+		return false;
+	}
+	a = Route.getVertexPos(a);
+	if (Route.removeVertex(a))
+		cout << "删除成功！\n\n";
+	write1();
+}
+void Manage::empty()
+{
+	Route.NodeTable = new Vertex[1000];
+	Route.numVertices = 0;
+	Route.numEdges = 0;
+}
